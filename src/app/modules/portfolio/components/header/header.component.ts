3e349e69f69d563c330301114bb41c43ba08b027
Arthur Lessa <thurlessaf@gmail.com>
2025-02-05 22:32:57 -0300

    WIP: Portfolio - Fix header and do about me section

--- src/app/modules/portfolio/components/header/header.component.ts
+++ src/app/modules/portfolio/components/header/header.component.ts
@@ -11,5 +11,7 @@ export class HeaderComponent {
 
   imgSrc = 'assets/img/Arthur2.png'
 
-  
+  public FirstAboutMeText = 'Olá, meu nome é Arthur. Sou desenvolvedor em formação, com experiência em testes automatizados, frontend e boas práticas de código. Trabalho com C#, Angular, HTML, TypeScript, SCSS e Playwright, buscando sempre soluções eficientes. Também tenho conhecimento em MySQL/DBeaver e controle de versão com Git e GitKraken.';
+  public SecondAboutMeText = 'Atualmente, foco no desenvolvimento frontend e na criação de interfaces dinâmicas. Também aprofundo meus conhecimentos em APIs com ASP.NET em C#, explorando boas práticas para desempenho e segurança. Gosto de resolver problemas de forma prática e aprender novas tecnologias constantemente.';
+  public ThirdAboutMeText = 'Se quiser trocar uma ideia sobre desenvolvimento web, APIs ou boas práticas de programação, estou sempre aberto a conversas!';
 }
